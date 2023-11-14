26088abaf37e00573f0d26401bdf66c929ae0c63
Laura López Gutiérrez <llopezgutie@uoc.edu>
2023-11-14 13:38:13 +0100

    1st commit - project almost finish

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,11 +6,75 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
+import Swiper from "swiper/swiper-bundle.min.mjs";
+
 /**
  * Write any other JavaScript below
  */
+/*const swiper = new Swiper(".swiper", {
+  speed: 400,
+  spaceBetween: 100,
+});*/
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+var swiper = new Swiper(".swiper-practicas-gallery", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 8000,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 40,
+    },
+  },
+});
+
+var swiper = new Swiper(".swiper-video-thumbnails", {
+  spaceBetween: 10,
+  slidesPerView: 2,
+  freeMode: true,
+  watchSlidesProgress: true,
+  autoplay: {
+    delay: 5000,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+var swiper2 = new Swiper(".swiper-video-main", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
